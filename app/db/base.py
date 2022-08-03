7d21392c46d26090e8eaa8aa65eab087df36cfd1
mmuellersoppart <mmuellersoppart@gmail.com>
2022-08-03 12:53:41 -0500

    add proposal model
    and run migration

--- app/db/base.py
+++ app/db/base.py
@@ -3,3 +3,4 @@
 from app.db.base_class import Base  # noqa: F401
 from app.db.tables.coupons import Coupon  # noqa: F401
 from app.db.tables.user import User  # noqa: F401
+from app.db.tables.proposal import Proposal  # noqa: F401
---
+++ app/db/tables/proposal.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+from app.db.base_class import Base
+
+class Proposal(Base):
+    __tablename__ = "proposal"
+
+    title = Column(String, unique=True)
+    body = Column(String)
+    date_created = Column(DateTime)
+    creator_id = Column("User", ForeignKey("user.id"))
+    #relationships
+    creator = relationship("User", back_populates="proposals")
+
